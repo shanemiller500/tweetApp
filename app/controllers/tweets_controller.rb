c1c046340a7cca833ddb449f8dfd951667a1446e
shane <shanemiller500@gmail.com>
2014-06-06 10:54:25 -0700

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,36 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+
+def create
+  @tweet = Tweet.create(tweet_params)
+  redirect_to root_path 
+end
+
+def show
+  @tweet = Tweet.find(params[:id])
+end
+
+def new
+  @tweet = Tweet.new
+end
+
+def edit
+  @tweet = Tweet.find(params[:id])
+end
+
+def destroy
+   @tweet = Tweet.find(params[:id])
+      @tweet.destroy
+    redirect_to root_path
+end
+
+private
+  def tweet_params
+    params.require(:tweet).permit(:message)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+  resources :tweets
+  root 'tweets#index'
+
+
+end
